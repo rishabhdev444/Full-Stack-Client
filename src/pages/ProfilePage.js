4056320a74391f7d953a6ed0cd52bf928368f32c
RISHABH DEV <53978955+rishabhdev444@users.noreply.github.com>
2024-03-29 23:41:11 -0700

    Update ProfilePage.js

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -22,17 +22,18 @@ function ProfilePage() {
     })
   },[])
 
-  return (
+ return (
     <div className='profilePageContainer'>
       <div className='basicInfo'>
-        <h1>Username:{username} </h1>
+        <h1>Username : <span>{username}</span> </h1>
         {
-          authState.username===username && <button onClick={()=>{navigate('/changepassword')}}>Change Password</button>
+          authState.username===username && <button className='changePwd' onClick={()=>{navigate('/changepassword')}}>Change Password</button>
           
         }
 
       </div>
       <div className='listOfPosts'>
+        
       {listOfPosts.map((value, key) => {
         return (
           <div key={key} className="post">
