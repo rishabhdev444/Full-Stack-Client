9e437fd76800976c70c03f3f53a2e37a1d740498
RISHABH DEV <53978955+rishabhdev444@users.noreply.github.com>
2024-03-29 23:41:27 -0700

    Update ProfilePage.js

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -22,41 +22,39 @@ function ProfilePage() {
     })
   },[])
 
- return (
-    <div className='profilePageContainer'>
-      <div className='basicInfo'>
-        <h1>Username : <span>{username}</span> </h1>
-        {
-          authState.username===username && <button className='changePwd' onClick={()=>{navigate('/changepassword')}}>Change Password</button>
-          
-        }
+  return (
+    <div className="registration">
+      <h1 className="login">Sign Up</h1>
+      <p className="loginSubtext">... to see the whole world on your finger tips!</p>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        <Form className="formContainer">
+          <label>Username </label>
+          <ErrorMessage name="username" component="span" />
+          <Field
+            autoComplete="off"
+            id="inputCreatePost"
+            name="username"
+            placeholder="(Ex. John123...)"
+          />
 
-      </div>
-      <div className='listOfPosts'>
-        
-      {listOfPosts.map((value, key) => {
-        return (
-          <div key={key} className="post">
-            <div className="title">{value.title}</div>
-            <div
-              className="body"
-              onClick={() => {
-                navigate(`/post/${value.id}`);
-              }}
-            >
-              {value.postText}
-            </div>
-            <div className="footer">
-              <div className="username"> {value.username}</div>
-              <div className="buttons">
-                <label>{value.Likes.length}</label>
-              </div>
-            </div>
-          </div>
-        );
-      })}
+          <label>Password </label>
+          <ErrorMessage name="password" component="span" />
+          <Field
+            autoComplete="off"
+            id="inputCreatePost"
+            type="password"
+            name="password"
+            placeholder="Your Password..."
+          />
+
+          <button type="submit"> Register</button>
+        </Form>
+      </Formik>
     </div>
-      </div>
   )
 }
 
